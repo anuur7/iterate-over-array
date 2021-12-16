4f967142cad7328d48dcf044b9d1b440c95b7d2b
Ayaanle Nur <Ayaanle-n@hotmail.com>
2021-12-16 20:34:06 +0000

    made some for loops by iterating over arrays

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //WRITE YOUR CODE BELOW
+
+let students = ["jake", "tom", "mike", "john", "harry"]
+
+console.log(students)
+
+for(let i = 0; i < students.length; i++){
+    console.log("Great to see you " + students[i])
+}
+
+
+
+let someNumbers = []
+
+for (let i = 0; i < 1000; i++) {
+  someNumbers.push(i) 
+}
+
+// New array where elements are the square of the elements in someNumbers
